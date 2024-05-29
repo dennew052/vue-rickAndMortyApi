2911cee87f39e01e4660211a0e3d25fd620a2faf
Denis <denis-velo.pro@mail.ru>
2024-05-29 20:50:58 +0500

    Добавлены исправления багов во время клика

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -12,17 +12,17 @@ const emit = defineEmits(['nextPage', 'previousPage', 'openPageByNum'])
   <div class="flex justify-center pb-6">
     <div
       class="flex text-xl [&>div]:w-12 [&>div]:h-12 [&>div]:border-l-2 [&>div]:bg-white [&>div]:flex [&>div]:items-center [&>div]:justify-center [&>div:active]:bg-gray-300">
-      <div :class="info && info.prev ? '' : ['text-gray-300', 'active:!bg-white']"
+      <div :class="info && info.prev ? 'cursor-pointer' : ['text-gray-300', 'active:!bg-white']"
            @click="() => emit ('openPageByNum', 1)">«
       </div>
-      <div :class="info && info.prev ? '' : ['text-gray-300', 'active:!bg-white']"
+      <div :class="info && info.prev ? 'cursor-pointer' : ['text-gray-300', 'active:!bg-white']"
            @click="() => emit ('previousPage')">‹
       </div>
       <div v-if="info" class="!w-fit pl-4 pr-4 active:!bg-white">{{ currentPage }} из {{ info.pages }}</div>
-      <div :class="info && info.next ? '' : ['text-gray-300', 'active:!bg-white']"
+      <div :class="info && info.next ? 'cursor-pointer' : ['text-gray-300', 'active:!bg-white']"
            @click="() => emit ('nextPage')">›
       </div>
-      <div :class="info && info.next ? '' : ['text-gray-300', 'active:!bg-white']"
+      <div :class="info && info.next ? 'cursor-pointer' : ['text-gray-300', 'active:!bg-white']"
            @click="() => emit ('openPageByNum', info.pages)">»
       </div>
     </div>
